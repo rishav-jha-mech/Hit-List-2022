60acbe39d0d8670886c17b7285f1a53d9b589e51
rishav-jha-mech <rishav.rj74@gmail.com>
2021-12-19 22:56:02 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const HIT = document.getElementById('hit')
+
+const killArray = [
+    {
+        "name": "Adnan",
+        "kill": false,
+    },
+    {
+        "name": "Subrata",
+        "kill": true,
+    },
+    {
+        "name": "Somrick",
+        "kill": true,
+    },
+    {
+        "name": "Saikat",
+        "kill": false,
+    },
+    {
+        "name": "Shreyan",
+        "kill": false,
+    },
+]
+
+for (let i = 0; i < killArray.length; i++) {
+    var div = document.createElement('div')
+    div.setAttribute("class","kill-human")
+    var green = "green"
+    if (killArray[i].kill != true) {
+        green = ""
+    }
+    div.innerHTML = `
+            <h1>${i+1}.</h1>
+            <h1><i class="fas ${green} fa-skull"></i>
+            &nbsp;&nbsp;
+            ${killArray[i].name}</h1>
+            <h1><i class="fas fa-skull-crossbones"></i></h1>
+            `
+    HIT.appendChild(div)
+}
+
+document.getElementById("footer").innerHTML = "Jinka naam S se start hot hai unhi ke saath lafda hota hai"
